38e0f437c231f6e35c839358471b4c244a99c44f
naru070605 <naru070605@gmail.com>
2023-10-21 16:12:33 +0900

    코밈

---
+++ 2590.py
@@ -0,0 +1,4 @@
+a = [0,0,0,0,0,0,0]
+for i in range(6):
+    b =int(input())
+    
--- 2592.py
+++ 2592.py
@@ -1,16 +1,20 @@
 a = []
 c = 0
 e = []
+
 for i in range(10):
     b = int(input())
     a.append(b)
     if e.count(b) == 0:
         e.append(b)
     c += b
+
 d = [0,0]
+
 for i in e:
     f = a.count(i)
     if d[1] < f:
         d = [i,f]
+        
 print(c//10)
 print(d[0])
